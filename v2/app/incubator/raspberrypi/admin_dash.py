055d1d14c0231e1a9f8c44e75fb10168981d75aa
Lucas S Eggers <l.eggers91@gmail.com>
2022-12-08 20:16:23 -0500

    update data in the background

--- v2/app/incubator/raspberrypi/admin_dash.py
+++ v2/app/incubator/raspberrypi/admin_dash.py
@@ -2,27 +2,12 @@
 # Please add to this file to customize your incubator!
 
 from dash import Dash, html, dcc
+from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 import sqlite3
 
 app = Dash(__name__)
-
-# Connect to database
-conn = sqlite3.connect("lucas-test.db")
-cursor = conn.cursor()
-
-# Read previous 1 week of data
-# TODO: set an app.layout function to reload data on page load
-# See https://dash.plotly.com/basic-callbacks
-df = pd.read_sql_query("""
-  SELECT datetime(time_sec, 'unixepoch', 'localtime') as time, temp, humidity from temp_humid_1m
-  WHERE time_sec > strftime('%s', datetime('now', '-6 days'))
-  ORDER BY time_sec DESC
-  """, conn)
-
-temp = px.line(df, x="time", y="temp", title="Temperature")
-humidity = px.line(df, x="time", y="humidity", title="Humidity")
 
 
 app.layout = html.Div(children=[
@@ -32,17 +17,39 @@ app.layout = html.Div(children=[
     html.Div(children='''
         Welcome to piano-bench
     '''),
-
-    dcc.Graph(
-        id='temp',
-        figure=temp
-    ),
-
-    dcc.Graph(
-        id='humidity',
-        figure=humidity
+    html.Div(id='interval-updated-graphs'),
+    # This component is used to refresh data in the background because it was
+    # too slow when rendering while serving a request. See `@app.callback` below
+    dcc.Interval(
+        id='interval-component',
+        interval=10 * 60 * 1000,  # ten minutes in milliseconds
+        n_intervals=0
     )
 ])
+
+
+def get_database_conn_and_cursor():
+    conn = sqlite3.connect("lucas-test.db")
+    cursor = conn.cursor()
+    return (conn, cursor)
+
+
+@app.callback(Output('interval-updated-graphs', 'children'),
+              Input('interval-component', 'n_intervals'))
+def update_metrics(_):
+    (conn, _) = get_database_conn_and_cursor()
+    # Read previous 1 week of data
+    df = pd.read_sql_query("""
+SELECT datetime(time_sec, 'unixepoch', 'localtime') as time, temp, humidity from temp_humid_1m
+WHERE time_sec > strftime('%s', datetime('now', '-6 days'))
+ORDER BY time_sec DESC
+""", conn)
+
+    temp = px.line(df, x="time", y="temp", title="Temperature")
+    humidity = px.line(df, x="time", y="humidity", title="Humidity")
+
+    return [dcc.Graph(id='temp', figure=temp), dcc.Graph(id='humidity', figure=humidity)]
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8000', debug=True)
